f1a8048d3b4795d72f86b033416c8b4b9f7b7a8c
me <me@GIFT>
2025-12-11 14:20:18 +0300

    Добавьте файлы проекта.

---
+++ ShoesShop/Pages/AuthPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoesShop.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbLogin.Text))
+            {
+                MessageBox.Show("Введите логин");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(pbPassword.Password))
+            {
+                MessageBox.Show("Введите Пароль");
+                return;
+            }
+
+            List<User> actualUsers = Entities.GetContext().User.ToList();
+
+            User user = actualUsers.Where(u=> u.login.Equals(tbLogin.Text)).FirstOrDefault();
+
+            if (user == null)
+            {
+                MessageBox.Show("Пользователь с таким именем не найден");
+                return;
+            }
+
+            if(user.password != pbPassword.Password)
+            {
+                MessageBox.Show("Введен неверный пароль");
+                return;
+            }
+
+            if(user.roleId == 1)
+            {
+                NavigationService.Navigate(new AdminPage());
+            }
+            else if (user.roleId == 2)
+            {
+                NavigationService.Navigate(new ManagerPage());
+            }
+            else if (user.roleId == 3)
+            {
+                NavigationService.Navigate(new UserPage());
+            }
+            else
+            {
+                MessageBox.Show("Ошибка идентификации роли пользователя");
+                return;
+            }
+        }
+    }
+}
---
+++ ShoesShop/Pages/ManagerPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoesShop.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ManagerPage.xaml
+    /// </summary>
+    public partial class ManagerPage : Page
+    {
+        public ManagerPage()
+        {
+            InitializeComponent();
+            lvProducts.ItemsSource = Entities.GetContext().Product.ToList();
+            cmbCategory.ItemsSource = Entities.GetContext().Category.ToList();
+            cmbDealer.ItemsSource = Entities.GetContext().Dealer.ToList();
+            cmbProducer.ItemsSource = Entities.GetContext().Producer.ToList();
+        }
+
+        private void UpdateList()
+        {
+            List<Product> products = Entities.GetContext().Product.ToList();
+
+            if(cmbCategory.SelectedItem != null)
+            {
+                products = products.Where(p=>p.categoryId == cmbCategory.SelectedIndex + 1).ToList();
+            }
+
+            if(cmbDealer.SelectedItem != null)
+            {
+                products = products.Where(p=>p.dealerId == cmbDealer.SelectedIndex + 1).ToList();
+            }
+
+            if(cmbProducer.SelectedItem != null)
+            {
+                products = products.Where(p=>p.producerId == cmbProducer.SelectedIndex + 1).ToList();
+            }
+
+            if(!string.IsNullOrEmpty(tbSearch.Text))
+            {
+                products = products.Where(p=>p.name.ToLower().Contains(tbSearch.Text.ToLower())).ToList();
+            }
+
+            lvProducts.ItemsSource = products;
+        }
+
+        private void cmbProducer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateList();
+        }
+
+        private void cmbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateList();
+        }
+
+        private void cmbCategory_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateList();
+        }
+
+        private void cmbDealer_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateList();
+        }
+
+        private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateList();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            cmbProducer.SelectedItem = null;
+            cmbDealer.SelectedItem = null;
+            cmbCategory.SelectedItem = null;
+            cmbSort.SelectedItem = null;
+            tbSearch.Text = "";
+            lvProducts.ItemsSource = Entities.GetContext().Product.ToList();
+        }
+    }
+}
